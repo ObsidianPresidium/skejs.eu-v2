d985759caf6908049436522254b83f9bf91e29cf
ObsidianPresidium <emil@aarestrup.nu>
2025-06-17 21:09:11 +0200

    add csnades

--- src/lib/CounterStrike.svelte
+++ src/lib/CounterStrike.svelte
@@ -7,4 +7,5 @@
     <p><a href="https://play.refrag.gg">Refrag</a></p>
     <p><a href="https://hltv.org">HLTV</a></p>
     <p><a href="https://steamcommunity.com/market/search?appid=730">Market</a></p>
+    <p><a href="https://csnades.gg">CSNades</a></p>
 </Box>
