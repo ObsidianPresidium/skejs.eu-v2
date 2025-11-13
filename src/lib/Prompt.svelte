7e74fe6aa9273bbb380352d00fab1559a399d9cb
ObsidianPresidium <emil@aarestrup.nu>
2025-11-13 15:27:59 +0100

    add prompt and link effect

---
+++ src/lib/Prompt.svelte
@@ -0,0 +1,34 @@
+<style lang="scss">
+    pre {
+        color: #2db878;
+    }
+
+    .blink {
+        color: white;
+        animation: blink 1s linear infinite;
+    }
+
+    @keyframes blink {
+        0% {
+            color: transparent;
+        }
+
+        49% {
+            color: transparent;
+        }
+
+        50% {
+            color: white;
+        }
+
+        100% {
+            color: white;
+        }
+    }
+</style>
+
+<script lang="ts">
+    let siteName = "aarestrup.dev";
+</script>
+
+<pre>emil@{siteName}<span style="color: white">:</span>~$ <span class="blink">█</span></pre>
